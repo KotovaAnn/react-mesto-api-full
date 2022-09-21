2ca886eebb1ac0dc0504e06d5883eae751c82a47
Kotova Anna <merpri@mail.ru>
2022-09-21 20:06:05 +0300

    fix: изменить путь

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'https://kotova.mesto.nomoredomains.sbs',
+    origin: 'https://api.kotova.mesto.nomoredomains.sbs',
     credentials: true,
   }),
 );
