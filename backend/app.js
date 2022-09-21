a1fe7d809cc938c546b967ab8324f92f6c3b6921
Kotova Anna <merpri@mail.ru>
2022-09-21 19:24:00 +0300

    fix: исправить адрес

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'http://kotova.mesto.nomoredomains.sbs',
+    origin: 'https://kotova.mesto.nomoredomains.sbs',
     credentials: true,
   }),
 );
