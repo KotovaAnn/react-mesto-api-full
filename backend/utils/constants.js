00f99d8a81de9aa7041f9ae0907acf0163c0f601
Kotova Anna <merpri@mail.ru>
2022-09-22 18:16:43 +0300

    fix: reg

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,2 +1,2 @@
-const regex = /(:?(?:https?:\/\/)?(?:www\.)?)?[-a-z0-9]+\.\w+/g;
+const regex = /(:?(?:https?:\/\/)?(?:www\.)?)?[-a-z0-9]+\.\w+(([/+\w\d\-._~:/?#[\]@!$&'()*+,;=]){1,})?/g;
 module.exports = regex;
